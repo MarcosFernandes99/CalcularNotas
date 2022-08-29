b45c006c94c89f028f743a64b516389c9238b30d
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-29 13:28:50 -0300

    Criando função para calcular notas de cem

--- CalcularNotas.js
+++ CalcularNotas.js
@@ -2,3 +2,16 @@
 // valor lido pode ser decomposto. Exiba o valor lido e a decomposição. Ex.: 576 // 5 nota(s) de 100 1 nota(s) de 50 2
 // notas(s) de 10 1 nota(s) de 5 1 nota(s) de 1
 
+var valor = prompt(`Insira um valor inteiro de reais`)
+console.log(`Valor a ser decomposto - R$${valor}`)
+
+CalcularNotasCem(valor)
+console.log(`Notas de CEM utilizadas - ${cem}`)
+
+
+
+function CalcularNotasCem(num1){
+    cem = parseInt(num1 / 100)
+    return sobraCinquenta = num1 - (100*cem)
+}
+
