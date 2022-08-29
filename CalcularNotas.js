f35c94f311c8d894b2b2518900c24d7d783d2392
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-29 13:23:17 -0300

    Criando algoritimo para calcular o menor valor possível de notas p/ decompor um valor inteiro de reais

---
+++ CalcularNotas.js
@@ -0,0 +1,4 @@
+// Faça um algoritmo que lê um valor inteiro de reais e calcula o menor valor possível de notas de 100, 50, 10, 5 e 1 que o
+// valor lido pode ser decomposto. Exiba o valor lido e a decomposição. Ex.: 576 // 5 nota(s) de 100 1 nota(s) de 50 2
+// notas(s) de 10 1 nota(s) de 5 1 nota(s) de 1
+
